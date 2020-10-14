6c80bb4c6fbe275241316e2c518f03b1de320f55
Mike <m_kanyar@encs.concordia.ca>
2020-10-14 14:52:29 -0400

    Adding unordered list with Js

--- myscript.js
+++ myscript.js
@@ -1,8 +1,28 @@
 
+// for (var i=0; i<querySelector("header").length){
+// console.log()
+// }
+// var x=document.querySelector("header").innerHTML;
+//     document.getElementById("title").innerHTML=x;
 
-var x=document.querySelector("header").innerHTML;
-    document.getElementById("title").innerHTML=x;
 
-   
+var headerNumber=document.getElementsByTagName("header").length;
+   for(var i=0; i<headerNumber;i++){
+   var hr=document.createElement('hr');
+   document.getElementById("title").appendChild(hr);
+    var x=document.getElementsByTagName("header")[i].innerText;
+    var id=x.replace(/\s+/g,"_");
+    var li=document.createElement('li');
+    var a= document.createElement('a');
+    var title=document.createTextNode(x);
+    a.appendChild(title);
+    a.href="#"+id;
+    a.className="nav-link";
+    li.appendChild(a);
+    document.getElementById("title").appendChild(li);
+     
+    console.log(id);
+    
+   }
 
 
